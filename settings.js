e0c5052fc8fb09f0e7247b147bddd2da029bbb8c
Botapp23 <160829273+Botapp23@users.noreply.github.com>
2024-03-19 11:39:20 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -15,32 +15,32 @@ global.mtl_language = false // multi language i hman dawn chuan true ah dah tur
 global.bot_language = 'lus' // I language code dah tur code hman i hriat loh chuan https://herbert70.blogspot.com/p/language-code-for-hbwabot.html?m=1 he link ah hian en thei a ni ang
 
 //info
-global.owner = ['919362640836'] // I phone number in thlak rawh
-global.ownernumber = '919362640836' // I phone number in thlak rawh
-global.ownername = "⭕" // I hming dah tel bawk la
+global.owner = ['917085596005'] // I phone number in thlak rawh
+global.ownernumber = '917085596005' // I phone number in thlak rawh
+global.ownername = "L i l thuamzz" // I hming dah tel bawk la
 global.location = "India, Mizoram, Aizawl" // I awmna hmun daha
-global.botname = "1 4 9 2 0" // I bot hming tur dah la
+global.botname = "Lil thuam zz" // I bot hming tur dah la
 global.ownerweb = "https://youtube.com/@HBMods_Channel"
 global.themeemoji = '🎲'
-global.creator = "919362640836@s.whatsapp.net" // @ symbol hmaa number hi i phone number in thlak rawh
+global.creator = "917085596005@s.whatsapp.net" // @ symbol hmaa number hi i phone number in thlak rawh
 
-global.ownerNumber = ["919362640836@s.whatsapp.net"]// @ symbol hmaa number hi i phone number in thlak rawh
+global.ownerNumber = ["917085596005@s.whatsapp.net"]// @ symbol hmaa number hi i phone number in thlak rawh
 global.prefa = ['','!','.','#','/']
 global.sessionName = 'session'
 
 // Sticker Pack Name
 global.packname = "Sticker By"
-global.author = "Ka huai ber"
+global.author = "lil thuamZz"
 global.hbmods = "https://herbert70.blogspot.com/"
 //_______________________________________________
 global.defaultpp = 'https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png?q=60'
 //messages
 global.mess = {
-    success: '\n*©1492***!', 
-    admin: '🤫',
-    botAdmin: '🤫',
-    owner: '🤫',
-    group: '🤫',
+    success: '\n*©lil thuam zz!', 
+    admin: '♥️',
+    botAdmin: '♥️',
+    owner: '♥️',
+    group: '♥️',
     private: 'Hei chu!',
     bot: 'Hei chu kei mah chauh hman thei tur a siam a ni',
 }
